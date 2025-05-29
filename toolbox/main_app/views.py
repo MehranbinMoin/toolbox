4cf6b83c60938a3b576e401b7f58764099bf8378
mehranbinmoin <mehranmoin@gmail.com>
2025-05-29 18:06:30 -0400

    Created a base.html file.

--- toolbox/main_app/views.py
+++ toolbox/main_app/views.py
@@ -3,3 +3,10 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse('<h1>toolbox</h1>')
+
+def about(request):
+    return render(request, 'about.html') 
+
+def tools (request):
+    return HttpResponse('<h1>All available tools</h1>')
+
