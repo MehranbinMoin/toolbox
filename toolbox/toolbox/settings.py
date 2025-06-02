5bbde8f132a8d4f1cc5e88bfd90068d116ba414c
mehranbinmoin <mehranmoin@gmail.com>
2025-06-02 18:36:10 -0400

    Added photo carousel styling on tool detail page.

--- toolbox/toolbox/settings.py
+++ toolbox/toolbox/settings.py
@@ -77,8 +77,11 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = [
+    BASE_DIR / "main_app" / "static",  
+]
 
 
 CLOUDINARY_STORAGE = {
